e368ef378fbaf06912012c4bfe5f7e1a4ca3934a
Jude Alexis Dy <dyjudealexis@gmail.com>
2025-05-15 22:40:22 +0800

    move css of fa & ei to assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './assets/css/font-awesome.min.css'
+import './assets/css/elegant-icons.css'
 import './assets/css/style.css'
 import './index.css'
 import App from './App.jsx'
